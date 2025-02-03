d88ed2e03c0d46961d08bf080d85726dacd579ed
jaiden <wailer.hideout.0l@icloud.com>
2025-02-02 19:08:35 -0600

    Further improvements to hero

--- src/lib/components/homepage/site-hero.svelte
+++ src/lib/components/homepage/site-hero.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import { Button } from '@/components/ui/button';
+    import { buttonVariants } from '@/components/ui/button';
     import ResponsiveDialog from '../global/responsive-dialog.svelte';
 
     const ctaText = 'Enter your skill levels';
 </script>
 
-<div class="hero h-screen py-12 -mt-16">
-    <div class="content-sizing flex flex-col justify-center items-start gap-12 mt-12">
+<div class="hero h-screen py-16 -mt-16">
+    <div class="content-sizing flex flex-col justify-center items-start gap-12 my-12">
         <h1 class="rs-font text-6xl text-primary">Welcome!</h1>
         <div class="text-lg flex flex-col gap-4">
             <p>
@@ -17,9 +17,9 @@
             <p>Powered by real-time Grand Exchange price stats.</p>
         </div>
 
-        <ResponsiveDialog title={ctaText}>
+        <ResponsiveDialog title={ctaText} triggerClass={buttonVariants({ variant: "default" })}>
             {#snippet trigger()}
-                <Button class="w-min">{ctaText}</Button>
+                <span class="primary">{ctaText}</span>
             {/snippet}
 
             {#snippet content()}
