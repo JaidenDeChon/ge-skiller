d1fc4d4200dc3735ec67e10ffe9cefd7e7c3658b
jaiden <wailer.hideout.0l@icloud.com>
2025-02-21 19:04:41 -0600

    Fix environment issue on dev

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,11 +1,26 @@
 import mongoose from 'mongoose';
 
+let user: string;
+let pw: string;
+let cluster: string;
+let host: string;
+let dbName: string;
+
+if (import.meta.env.PROD) {
+    user = process.env.VITE_MONGO_USERNAME ?? '';
+    pw = process.env.VITE_MONGO_PASSWORD ?? '';
+    cluster = process.env.VITE_MONGO_DB_CLUSTER_NAME ?? '';
+    host = process.env.VITE_MONGO_DB_HOST ?? '';
+    dbName = process.env.VITE_MONGO_DB_DB_NAME ?? '';
+} else {
+    user = import.meta.env.VITE_MONGO_USERNAME ?? '';
+    pw = import.meta.env.VITE_MONGO_PASSWORD ?? '';
+    cluster = import.meta.env.VITE_MONGO_DB_CLUSTER_NAME ?? '';
+    host = import.meta.env.VITE_MONGO_DB_HOST ?? '';
+    dbName = import.meta.env.VITE_MONGO_DB_DB_NAME ?? '';
+}
+
 // MongoDB connection string setup.
-const user = process.env.VITE_MONGO_USERNAME;
-const pw = process.env.VITE_MONGO_PASSWORD;
-const cluster = process.env.VITE_MONGO_DB_CLUSTER_NAME;
-const host = process.env.VITE_MONGO_DB_HOST;
-const dbName = process.env.VITE_MONGO_DB_DB_NAME;
 const prepend = 'mongodb+srv';
 const append = '?retryWrites=true&w=majority';
 const connectionString = `${prepend}://${user}:${pw}@${cluster}.${host}/${dbName}${append}`;
