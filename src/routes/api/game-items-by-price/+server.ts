6aad8f201a1135c5ae254f06afb26af1ef012df2
jaiden <wailer.hideout.0l@icloud.com>
2025-03-01 00:37:25 -0600

    Deleted now-unused files

--- src/routes/api/game-items-by-price/+server.ts
+++
@@ -1,7 +0,0 @@
-import { getGameItemsByPrice } from '$lib/helpers/game-items';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async () => {
-    const gameItems = await getGameItemsByPrice();
-    return new Response(JSON.stringify(gameItems));
-}
