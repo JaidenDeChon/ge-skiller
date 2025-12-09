7b686fa843d5655d09b6a38afc1f58f56c5b701a
jaiden <wailer.hideout.0l@icloud.com>
2025-12-09 13:27:30 -0600

    Install Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
+import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 export default ts.config(
@@ -11,21 +13,15 @@ export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
   ...svelte.configs["flat/recommended"],
+  prettier,
+  ...svelte.configs.prettier,
   {
     languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
-	}
+      globals: { ...globals.browser, ...globals.node }
+    }
   },
   {
     files: ["**/*.svelte"],
-
-    languageOptions: {
-	  parserOptions: {
-	    parser: ts.parser
-	  }
-	}
+    languageOptions: { parserOptions: { parser: ts.parser } }
   }
 );
